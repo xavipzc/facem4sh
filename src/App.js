40978d953058488e0b0638f733c7d00e884c11e0
Xavier Pouzenc <x.pouzenc@pzc.fr>
2019-01-25 23:03:56 +0100

    add sass to the project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
 import Home from './Pages/Home'
-import './App.css'
 
 class App extends Component {
 	render() {
